c7fb6be572eec88675434b92b3f691de5e8a1c40
eva.masakova <eva.masakova@post.cz>
2022-09-16 10:23:53 +0200

    changed "Martin" to "Eschus" changed "Skola" to "Parkoviste" changed "Zdenka" to "Honza" changed "kiosek" to "Kabinet "

--- src/com/rpg/Main.java
+++ src/com/rpg/Main.java
@@ -12,7 +12,7 @@ public class Main {
         enemy = new Entity("Honza", 1000, 1000);
         Arena arena = new Arena("Parkoviste");
         arena.fight(player, enemy);
-        arena = new Arena("Kabinet o_o");
+        arena = new Arena("Kabnet o_o");
         player.setHp(100);
         player.setDmg(10);
         enemy.setHp(1000);
